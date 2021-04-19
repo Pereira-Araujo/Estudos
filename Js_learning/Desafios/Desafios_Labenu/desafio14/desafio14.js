0710dd8a0f6885e54867331d206fb55c5fdb7f38
Adriano P. de Araujo <60116988+Pereira-Araujo@users.noreply.github.com>
2021-04-19 17:29:46 -0300

    Delete Js_learning/Desafios/Desafios_Labenu directory

--- Js_learning/Desafios/Desafios_Labenu/desafio14/desafio14.js
+++
@@ -1,9 +0,0 @@
-// Escreva um algoritmo para ler uma temperatura em graus Fahrenheit, 
-// calcular e escrever o valor correspondente em graus Celsius 
-// (baseado na fórmula abaixo): (0 °C × 9/5) + 32 = 32 °F
-
-
-const conversion = (CelciusValue) => {
-    const FahrenheitValue = (Number(CelciusValue) * (9/5) + 32)
-    return FahrenheitValue 
-}
