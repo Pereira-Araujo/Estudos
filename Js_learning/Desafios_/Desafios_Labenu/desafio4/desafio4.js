486e888aa50a9b06c72dde52ad7e64c17b21cd61
Pereira-Araujo <araujo_ir@yahoo.com>
2021-04-19 17:43:49 -0300

    desafio_4

--- Js_learning/Desafios_/Desafios_Labenu/desafio4/desafio4.js
+++ Js_learning/Desafios_/Desafios_Labenu/desafio4/desafio4.js
@@ -1,7 +1,6 @@
 // Faça um programa que leia um valor e imprima a mensagem "É maior que 10" se o valor
 // lido for maior que 10,
-// caso contrário imprima "Não é maior que 10"
-
+// caso contrário imprima 'Não é maior que 10'
 function maiorOuMenor(valor) {
   if (valor < 10) {
     console.log("Valor menor do que 10");
