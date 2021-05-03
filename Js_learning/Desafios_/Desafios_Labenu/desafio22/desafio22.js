197d721df991275283da0e68bac3d641b1dbc892
Pereira-Araujo <araujo_ir@yahoo.com>
2021-05-03 19:11:20 -0300

    Desafio_22

---
+++ Js_learning/Desafios_/Desafios_Labenu/desafio22/desafio22.js
@@ -0,0 +1,10 @@
+/*
+Faça uma função que receba uma string e devolva um array com seus caracteres. 
+Se o input for *escola*, a saída deve ser [e, s, c, o, l, a] 
+*/
+
+const desafio = (string) => {
+  console.log(string.split(''))
+}
+
+desafio('Labenu')
