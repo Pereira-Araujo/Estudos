50b50ba60a38c2770d0c9389a1bb310f8b887672
Pereira-Araujo <araujo_ir@yahoo.com>
2021-05-14 17:16:39 -0300

    Detalhes na estilização

--- Js_learning/Cursos/Udemy_Js-Fullstack/aula67/Lista_d_tarefas/script.js
+++ Js_learning/Cursos/Udemy_Js-Fullstack/aula67/Lista_d_tarefas/script.js
@@ -1,7 +1,7 @@
+
 const inputTask = document.querySelector('.card__create-task')
 const add = document.querySelector('.card__button')
 const list = document.querySelector('.card__list')
-
 
 
 const createLi = ()=>{
@@ -10,23 +10,28 @@ const createLi = ()=>{
   
 }
 
+const clearInput = () =>{
+    inputTask.value = ''
+    inputTask.focus()
+}
+
 const createTask = (textInput)=>{
     const li = createLi()
 
     li.innerText = textInput
     list.appendChild(li)
+    clearInput()
 
 }
 
-
-
-
-add.addEventListener('click',function(){
+add.addEventListener('click',()=>{
     if(!inputTask.value) return
-
     createTask(inputTask.value)
+})
 
-}
+inputTask.addEventListener('keypress',(event)=>{
+    if(!inputTask.value) return
+    if(event.keyCode===13){
+    createTask(inputTask.value)}
+})
 
-
-)
