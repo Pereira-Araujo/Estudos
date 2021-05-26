78f055675425301767f0390ebb6fc720b36a03cc
Adriano P. de Araujo <araujo_ir@yahoo.com>
2021-05-26 14:42:12 -0300

    Libs : nodemon & morgan

--- Node_learning/Cursos/Node_Maransatto/rest-api/app.js
+++ Node_learning/Cursos/Node_Maransatto/rest-api/app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app= express()
 const router = express.Router()
+const morgan = require('morgan')
 
 const productsRoutes = require('./routes/products')//import 
 const resquestsRoutes = require('./routes/requests')//import 
 
+app.use(morgan('dev'))
 
 app.use('/products',productsRoutes)
 app.use('/requests',resquestsRoutes)
