e7e6a4e6ca5e7a19896b53cfea326591f86f1460
Adriano P. de Araujo <araujo_ir@yahoo.com>
2021-05-26 14:55:45 -0300

    Libs: body-parser

--- Node_learning/Cursos/Node_Maransatto/rest-api/app.js
+++ Node_learning/Cursos/Node_Maransatto/rest-api/app.js
@@ -2,15 +2,33 @@ const express = require('express')
 const app= express()
 const router = express.Router()
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 
 const productsRoutes = require('./routes/products')//import 
 const resquestsRoutes = require('./routes/requests')//import 
 
 app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended:false})) // dados simples
+app.use(bodyParser.json()) //tipo json
 
-app.use('/products',productsRoutes)
+app.use('/products',productsRoutes) //rotas
 app.use('/requests',resquestsRoutes)
 
 
+// Erro para rotas não encontradas
+app.use((res,req,next)=>{
+    const error = new Error('Rota não encontrada')
+    error.status = 404
+    next(error)
+})
+
+app.use((error,req, res, next)=>{
+    res.status(error.status || 500)
+    return res.send({
+        error:{
+            message:error.message
+        }
+    })
+})
 
 module.exports = app
