ee8f09e69c0847b9bbf2043ebe6c358a345afecc
Adriano P. de Araujo <araujo_ir@yahoo.com>
2021-05-27 22:37:44 -0300

    Integração com banco de dados dos métodos POST & GET

--- Node_learning/Cursos/Node_Maransatto/rest-api/routes/products.js
+++ Node_learning/Cursos/Node_Maransatto/rest-api/routes/products.js
@@ -1,24 +1,60 @@
 const express = require("express");
 const router = express.Router();
+const mysql = require("../mysql").pool;
 
 router.get("/", (req, res, next) => {
-  res.status(200).send({
-    message: "Retorna todos os produtos",
-  });
+  // res.status(200).send({
+  //   message: "Retorna todos os produtos",
+  // });
+
+  mysql.getConnection((error,conn)=>{
+    if(error){
+      return res.status(500).send({error:error})
+    }
+    conn.query(
+      "SELECT * FROM products;",
+      (error,response,fields)=>{
+        if(error){
+          return res.status(500).send({error:error})
+        }
+        return res.status(200).send({response:response})
+
+      }
+    )
+
+  })
 });
+
+
 
 router.post("/", (req, res, next) => {
-  const product = {
-    name:req.body.name,
-    price:req.body.price,
-    image:req.body.image,
-  }
-  res.status(201).send({
-      message: "Adiciona novos produtos",
-      productCreated:product
+
+  mysql.getConnection((error, conn) => {
+    if(error){
+      return res.status(500).send({error:error})
+    }
+    conn.query(
+      "INSERT INTO products(name,price,image) VALUES(?,?,?)",
+      [req.body.name, req.body.price, req.body.image],
+      (error, response, field) => {
+
+        conn.release();
+
+        if (error) {
+          return res.status(500).send({
+            error: error,
+            response: null,
+          });
+        }
+
+        res.status(201).send({
+          message: "Produto inserido com sucesso",
+          id_product: response.insertId,
+        });
+      }
+    );
   });
 });
-
 
 router.get("/:id_product", (req, res, next) => {
   const id = req.params.id_product;
@@ -41,6 +77,5 @@ router.delete("/", (req, res, next) => {
     message: "Deleta os produtos",
   });
 });
-
 
 module.exports = router;
