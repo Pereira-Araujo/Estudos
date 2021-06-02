2d7cb9de257b18a12d6a173e5a325d91738ca0c1
Adriano P. de Araujo <araujo_ir@yahoo.com>
2021-06-02 16:26:31 -0300

    testes com formik & yup

--- React_Js_learning/Revisao/app/src/components/LoginForm/index.jsx
+++ React_Js_learning/Revisao/app/src/components/LoginForm/index.jsx
@@ -1,25 +1,44 @@
-import { Formik, Form, Field } from "formik";
+import {useState} from 'react'
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { signIn } from "../../utils/validations";
 
 const LoginForm = (props) => {
+
+  const enter = (values) => {
+    setInform(values)
+
+    alert(inform);
+  };
+
+  const [inform, setInform] =useState('')
+
+ 
+
   return (
     <>
       <h1>{props.title}</h1>
       <Formik
+        validationSchema={signIn}
         initialValues={{
           login: "",
           password: "",
         }}
+        onSubmit={()=>enter(props.values.login)}
       >
         {() => (
           <Form>
             <div>
               <label>Login</label>
-              <Field id="login" name="login" type="text" />
+              <Field id="login"  name="login" type="text" />
+              <ErrorMessage name="login" />
             </div>
             <div>
               <label>Senha</label>
               <Field id="password" name="password" type="text" />
+              <ErrorMessage name="password"/>
             </div>
+
+            <button>Entrar</button>
           </Form>
         )}
       </Formik>
---
+++ React_Js_learning/Revisao/app/src/utils/validations.js
@@ -0,0 +1,10 @@
+import * as Yup from "yup";
+
+
+export const signIn = Yup.object().shape({
+  login: Yup.string().email("Deve ser um email"),
+  password: Yup.string()
+    .min(6, "Deve ter mais de 6 caracteres")
+    .required("Não pode estar vazio"),
+});
+
