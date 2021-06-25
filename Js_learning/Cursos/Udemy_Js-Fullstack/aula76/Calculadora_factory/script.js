f158b1a970e630d5985afaa4ee83df585a4d4b0e
Adriano P. de Araujo <araujo_ir@yahoo.com>
2021-06-24 21:04:21 -0300

    Aula 76 :Funcionalidades básicas

---
+++ Js_learning/Cursos/Udemy_Js-Fullstack/aula76/Calculadora_factory/script.js
@@ -0,0 +1,27 @@
+const createCalculator = () => {
+  return {
+    display: document.querySelector(".calculator__display"),
+
+    initial() {
+      this.clickButtons();
+    },
+
+    clickButtons() {
+      document.addEventListener(
+        "click",
+        function (event) {
+          const element = event.target;
+          if (element.classList.contains("calculator__button")) {
+            this.buttonToDisplay(element.innerText);
+          }
+        }.bind(this)
+      );
+    },
+
+    buttonToDisplay(text) {
+      this.display.value += text;
+    },
+  };
+};
+const calculator = createCalculator();
+calculator.initial();
